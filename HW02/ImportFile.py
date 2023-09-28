fa165017895dee3a9515f13ade498856a249a95b
Akshay Kumar <akshaykumar@wisc.edu>
2023-09-28 18:43:15 -0500

    final code HW02

--- HW02/ImportFile.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-# Text file data converted to integer data type
-File_data = np.loadtxt('HomeworkData/Druns.txt', dtype=float)
-print(File_data.shape)
--- HW02/LagrangeInterpolation_4.py
+++ HW02/LagrangeInterpolation_4.py
@@ -1,24 +1,31 @@
 import numpy as np
 import scipy.interpolate as interpolate
-
+import matplotlib.pyplot as plt
+from numpy.polynomial.polynomial import Polynomial
 def log_mean_squared_error_fun(y,ypred):
     LMSE = np.mean((np.log(y)-np.log(ypred))**2)
     return LMSE
+    
 #generate the training set
-a, b = 0, 2 * np.pi
+a, b = np.pi/4,3*np.pi/4
 n = 100
 x = np.random.uniform(a, b, n)
 y = np.sin(x)
 
+fitDegree = 4
 #building the Lagrange interpolation model
-lagrange_model = interpolate.lagrange(x, y)
+lagrange_model = interpolate.lagrange(x[0:fitDegree], y[0:fitDegree])
 
 #generate the test set
 x_test = np.random.uniform(a, b, n)
 y_test = np.sin(x_test)
 
+# plt.figure()
+# plt.hist(x)
+
 #compute train and test errors
 y_train_predicted = lagrange_model(x)
+# y_train_predicted = Polynomial(lagrange_model.coef[::-1])(x)
 train_error = log_mean_squared_error_fun(y, y_train_predicted)
 y_test_predicted = lagrange_model(x_test)
 test_error = log_mean_squared_error_fun(y_test, y_test_predicted)
@@ -27,7 +34,7 @@ print("Train Error:", train_error)
 print("Test Error:", test_error)
 
 #Gaussian noise
-std_devs = [0.1, 0.5, 1.0]  #standard deviations for noise
+std_devs = [0.01,0.05, 0.2]  #standard deviations for noise
 
 for std_dev in std_devs:
     #add Gaussian noise 
@@ -37,7 +44,7 @@ for std_dev in std_devs:
     y_test_noisy = np.sin(x_test_noisy)
 
     #build the Lagrange interpolation model
-    lagrange_model_noisy = interpolate.lagrange(x_noisy, y_noisy)
+    lagrange_model_noisy = interpolate.lagrange(x_noisy[0:fitDegree], y_noisy[0:fitDegree])
 
     # Error calculation
     y_train_predicted_noisy = lagrange_model_noisy(x_noisy)
