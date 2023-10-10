5a53f7d7f273ea6df7666efc230005f8b71c3bfa
Akshay Kumar <akshaykumar@wisc.edu>
2023-10-10 02:48:27 -0500

    big prob

---
+++ HW03/Q2_P4.py
@@ -0,0 +1,56 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('hw3Data/emails.csv')
+
+X = df.drop(columns=['Prediction']).values
+X = X[:,1::]
+y = df['Prediction'].values
+
+def euclidean_distance(x1, x2):
+    return np.linalg.norm(x1 - x2)
+
+#kNN function
+def kNN(X_train, y_train, test_point, k):
+    distances = [euclidean_distance(train_point, test_point) for train_point in X_train]
+    k_indices = np.argsort(distances)[:k]
+    k_nearest_labels = [y_train[i] for i in k_indices]
+    
+    # most common label in the kNN
+    unique_labels, counts = np.unique(k_nearest_labels, return_counts=True)
+    most_common_label = unique_labels[np.argmax(counts)]
+    return most_common_label
+
+#accuracy calculation
+def accuracy(y_true, y_pred):
+    return np.mean(y_true == y_pred)
+
+# 5-Fold CV
+folds = [(0, 1000), (1000, 2000), (2000, 3000), (3000, 4000), (4000, 5000)]
+k_values = [1, 3, 5, 7, 10]
+results = []
+
+for k in k_values:
+    accuracies = []
+    for fold in folds:
+        test_X = X[fold[0]:fold[1]]
+        test_y = y[fold[0]:fold[1]]
+        train_X = np.concatenate((X[:fold[0]], X[fold[1]:]), axis=0)
+        train_y = np.concatenate((y[:fold[0]], y[fold[1]:]), axis=0)
+        
+        predictions = [kNN(train_X, train_y, test_point, k) for test_point in test_X]
+        accuracies.append(accuracy(test_y, predictions))
+    results.append(np.mean(accuracies))
+
+#results
+plt.plot(k_values, results, marker='o')
+plt.xlabel('k')
+plt.ylabel('Average Accuracy')
+plt.title('Average Accuracy vs. k')
+plt.show()
+
+# Print
+for k, accuracy in zip(k_values, results):
+    print(f"k = {k}: Average Accuracy = {accuracy:.4f}")
---
+++ HW03/Q2_P5.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('hw3Data/emails.csv')
+
+X = df.drop(columns=['Prediction']).values
+X = X[:,1::]
+y = df['Prediction'].values
+
+# Split data
+train_X = X[:4000]
+train_y = y[:4000]
+test_X = X[4000:]
+test_y = y[4000:]
+
+def euclidean_distance(x1, x2):
+    return np.linalg.norm(x1 - x2)
+
+# kNN
+def kNN(X_train, y_train, test_points, k):
+    predictions = []
+    for test_point in test_points:
+        distances = [euclidean_distance(train_point, test_point) for train_point in X_train]
+        k_indices = np.argsort(distances)[:k]
+        k_nearest_labels = [y_train[i] for i in k_indices]
+        most_common_label = max(set(k_nearest_labels), key=k_nearest_labels.count)
+        predictions.append(most_common_label)
+    return predictions
+
+# Logistic Regression
+def logistic_regression(X, y, num_iterations, learning_rate):
+    n_samples, n_features = X.shape
+    # initialize weights and bias
+    weights = np.zeros(n_features)
+    bias = 0
+
+    #GD
+    for _ in range(num_iterations):
+        linear_model = np.dot(X, weights) + bias
+        predicted_y = 1 / (1 + np.exp(-linear_model))
+        
+        # Compute gradients
+        dw = (1/n_samples) * np.dot(X.T, (predicted_y - y))
+        db = (1/n_samples) * np.sum(predicted_y - y)
+
+        # Update weights and bias
+        weights -= learning_rate * dw
+        bias -= learning_rate * db
+    
+    return weights, bias
+
+def predict_logistic(X, weights, bias):
+    linear_model = np.dot(X, weights) + bias
+    return 1 / (1 + np.exp(-linear_model))
+
+# Train kNN and make predictions
+knn_predictions = kNN(train_X, train_y, test_X, 5)
+
+# Train Logistic Regression and make predictions
+weights, bias = logistic_regression(train_X, train_y, num_iterations=1000, learning_rate=0.001)
+logistic_predictions = predict_logistic(test_X, weights, bias)
+
+# ROC Curve
+from sklearn.metrics import roc_curve
+
+knn_probs = [1 if i == 1 else 0 for i in knn_predictions]
+fpr_knn, tpr_knn, _ = roc_curve(test_y, knn_probs)
+fpr_logistic, tpr_logistic, _ = roc_curve(test_y, logistic_predictions)
+
+plt.figure(figsize=(10, 7))
+plt.plot(fpr_knn, tpr_knn, label="kNN")
+plt.plot(fpr_logistic, tpr_logistic, label="Logistic Regression")
+plt.xlabel("False Positive Rate")
+plt.ylabel("True Positive Rate")
+plt.title("ROC Curve")
+plt.legend()
+plt.show()
